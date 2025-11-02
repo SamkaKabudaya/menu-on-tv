9b90769ed3fb24eec1d76414c88b79920bf5d213
SamkaKabudaya <60573016+SamkaKabudaya@users.noreply.github.com>
2025-11-02 13:40:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ async def poster_callback(code: str, account: str = None):
         "application_secret": "eb7286874ae3e6c82d2d688711cb4950",
         "grant_type": "authorization_code",
         "code": code,
-        "redirect_uri": "https://mysite.onrender.com/menu-on-tv"  # см. ниже
+        "redirect_uri": "https://menu-on-tv.onrender.com/menu-on-tv"  # см. ниже
     }
     async with httpx.AsyncClient() as client:
         r = await client.post(url, data=data)
         token_data = r.json()
+
     return {"message": "Авторизация успешна", "token": token_data}
